c9a587646ce378ffe565c11deecbff6331d0420a
Night8Guy <robert@lionsroad.com>
2023-05-22 13:04:01 -0500

    tasks

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""This module writes a class 'Square'.
-This class will initialize a size for Square."""
+"""class Square that defines a square"""
 
 
 class Square:
-    """Class that initializes size as a private instance attribute"""
+    """class Square that defines a square"""
+    __size = None
+
     def __init__(self, size):
+        """Initialization with size"""
         self.__size = size
-        """init func initializes Square object and gives it a size"""

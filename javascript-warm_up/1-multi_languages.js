9a37a8180c4419fda0bb9a88d5a7333cd93f5c49
Night8Guy <robert@lionsroad.com>
2023-07-31 09:33:48 -0500

    javascripts

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let theString = 'C is fun';
+console.log(theString);
+theString = 'Python is cool';
+console.log(theString);
+theString = 'JavaScript is amazing';
+console.log(theString);

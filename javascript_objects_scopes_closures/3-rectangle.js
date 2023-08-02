928be69d671c192865fa5e64134d0c1cd591a35d
Night8Guy <robert@lionsroad.com>
2023-08-01 21:57:31 -0500

    tests

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if (w >= 1 && h >= 1) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      let xCounter;
+      let yCounter;
+      let output = '';
+      for (xCounter = 0; xCounter < this.width; xCounter++) {
+        output += 'X';
+      }
+      for (yCounter = 0; yCounter < this.height; yCounter++) {
+        console.log(output);
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
